51f30beb697b988179b728775af900c22dabdf37
Mark Burrell <markrb0609@gmail.com>
2017-11-28 11:11:30 -0700

    made constants readable

--- src/ev3/liabraries/Constants.java
+++ src/ev3/liabraries/Constants.java
@@ -1,7 +1,17 @@
 package ev3.liabraries;
 
 public class Constants {
-	public  final int delay=30,turn1=300,turnSpeed2=250,strightSpeed1=300,turn2=150,straight2=800,turn3=150,straight3=600,straight4=600, turn4=300,straight5=800;
+	public  final int delay=30;
+	public  final int turn1=300;
+	public  final int turnSpeed2=250;
+	public  final int strightSpeed1=300;
+	public  final int turn2=150;
+	public  final int straight2=800;
+	public  final int turn3=150;
+	public  final int straight3=600;
+	public  final int straight4=600;
+	public  final int turn4=300;
+	public  final int straight5=800;
 	public  final float lightThresh=(float) 0.1;
 	public Constants()
 	{
